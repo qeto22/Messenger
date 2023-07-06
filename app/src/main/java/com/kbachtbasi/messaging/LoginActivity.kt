65e2e3fd9551dc63ee7d008c762630844bc6d7e1
Ketevan Bachalashvili <kbach19@freeuni.edu.ge>
2023-07-07 01:24:30 +0400

    viewbinding and sign up page

--- app/src/main/java/com/kbachtbasi/messaging/LoginActivity.kt
+++ app/src/main/java/com/kbachtbasi/messaging/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.kbachtbasi.messaging
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kbachtbasi.messaging.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.signUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

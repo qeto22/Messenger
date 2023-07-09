1eaa6179ad21d14e987b07109e853492b0fffbe1
Ketevan Bachalashvili <kbach19@freeuni.edu.ge>
2023-07-09 23:25:55 +0400

    Added bottom navigation bar and fragments

--- app/src/main/java/com/kbachtbasi/messaging/MainActivity.kt
+++ app/src/main/java/com/kbachtbasi/messaging/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.kbachtbasi.messaging
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kbachtbasi.messaging.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.mainActivityFragmentContainer.id, HomeFragment())
+            .commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(binding.mainActivityFragmentContainer.id, HomeFragment())
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(binding.mainActivityFragmentContainer.id, SettingsFragment())
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
